f53a4cc21905a384fe36b1112bc04a2f3ba38d10
ZiaynGZiyaNG <chen971023@gmail.com>
2025-05-30 00:26:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
-# main.py
-import os
 from parser_text import parse_program
-from compiler_custom import compile_to_custom_code
+from interpreter import run
 
-code = """
---以下開使打code
-"""
+code = '''
 
-desktop_path = os.path.join(os.path.expanduser("~"), "Desktop")
-output_file = os.path.join(desktop_path, "output.txt")
+'''
 
 ast = parse_program(code.strip().splitlines())
-compile_to_custom_code(ast, output_file)
+run(ast)
